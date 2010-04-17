e2d38ce9538531971f91a8ea0c9544bb282db7c0
flop25 <flop25@piwigo.org>
2010-04-17 17:01:43 +0000

    Pure_green_nature 2° step adding files
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_green_nature@5910 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,9 @@
+<?php
+$themeconf = array(
+  'name'         => 'Pure_green_nature',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
