744b9ff92cd583d504e016c1fa77042ef12d48a3
flop25 <flop25@piwigo.org>
2010-05-26 12:42:44 +0000

    first version 1.0 ; adding adtional info in themeconf.inc.php
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_green_nature@6370 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Theme Name: Pure_green_nature
+Version: 1.0.0
+Description: Rounded corners and a green inspired from the nature, these are the two characteristics of this style.
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=385
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
 $themeconf = array(
   'name'         => 'Pure_green_nature',
   'parent'        => 'Pure_default',
